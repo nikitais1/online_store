7c59e8a921306cb5947ccdec63450df7ab19a3d4
Nikita <nikita.isaev2001@mail.ru>
2024-01-10 01:28:05 +0300

    hw-3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, prod
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('prod/<int:pk>', prod, name='prod'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def prod(request, pk):
+    prod_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(id=pk),
+        'title': f'{prod_item.product_name}',
+    }
+    return render(request, 'catalog/prod.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты',
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
@@ -14,4 +32,4 @@ def contacts(request):
         with open('feedback.txt', 'w') as f:
             f.write(f'User_name: {name}\nContact_number: {phone}\n"{message}"\n')
 
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
