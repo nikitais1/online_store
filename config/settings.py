047c304b368be03841101e54bb0b5709b474b89e
Nikita <nikita.isaev2001@mail.ru>
2024-02-20 08:42:37 +0300

    hw-9 * 2.0

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
